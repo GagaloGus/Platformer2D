65e800d7d2c1f50a4abe62fa99be2104b7b40b5d
smoral1 <soledadmoral13@gmail.com>
2026-01-09 09:27:02 +0100

    Creado script GameManager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    public static GameManager Instance;
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
