67135066be0d247df348592d08b066b5ac1d02e8
smoral1 <soledadmoral13@gmail.com>
2026-01-28 09:48:00 +0100

    Change codigo gameManager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,17 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public enum GameState { Playing, Paused, Victory, GameOver }
 
 public class GameManager : MonoBehaviour
 {
 
     public static GameManager Instance;
 
-    public enum GameState { Menu, Playing, Paused, GameOver}
-    public GameState currentState;
 
-    private int score = 0;
-    //private int playerLives = 3;
+    [Header("Game State")]
+    public GameState gameState = GameState.Playing;
+
+    [Header("Score")]
+    private int score;
 
     private void Awake()
     {
@@ -23,54 +27,47 @@ public class GameManager : MonoBehaviour
         {
             Destroy(gameObject);
         }
-        DontDestroyOnLoad(gameObject);
+       
     }
 
     void Start()
     {
-        ChangeState(GameState.Menu);
-    }
-
-    public void ChangeState(GameState newState)
-    {
-        currentState = newState;
-    }
-
-    public void StartGame()
-    {
+        Time.timeScale = 1f;
         score = 0;
-        ChangeState(GameState.Playing);
-        
+        gameState = GameState.Playing;
     }
 
-    public void PauseGame()
-    {
-        Time.timeScale = 0f;
-        ChangeState(GameState.Paused);
-    }
-
-    public void ResumeGame()
-    {
-        Time.timeScale = 0f;
-        ChangeState(GameState.Playing);
-    }
-
-    public void AddScore (int amount)
+    public void AddScore(int amount)
     {
         score += amount;
     }
 
-    /* public void PlayerDie()
-     {
-         playerLives--;
+    public void PauseGame()
+    {
+        if (gameState != GameState.Playing) return;
 
-         if (playerLives <= 0)
-         {
-             ChangeState(GameState.GameOver);
-         }
-     }*/
+        gameState = GameState.Paused;
+        Time.timeScale = 0f;
+    }
 
-    public int GetScore() => score;
-    //public int GetLives() => playerLives;
+    public void ResumeGame()
+    {
+        if (gameState != GameState.Paused) return;
+
+        gameState = GameState.Playing;
+        Time.timeScale = 1f;
+    }
+
+    public void RestartGame()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void Victoria()
+    {
+        gameState = GameState.Victory;
+        Time.timeScale = 0f;
+    }
 
 }
